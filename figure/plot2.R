4ca5427f7c18a64c8b6cc9b03eb8d6bc1e28d6cf
Anand <anand.chellappa@gmail.com>
2016-02-22 22:58:05 -0800

    Adding R Code

---
+++ figure/plot2.R
@@ -0,0 +1,19 @@
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if (!file.exists("./data/household_power_consumption.txt")) 
+{
+  if(!file.exists("./data")) { dir.create("./data")}
+  download.file(fileurl, destfile="./data/power_data.zip")
+  unzip("./data/power_data.zip", overwrite = T, exdir = "./data")
+}
+
+data <- read.table("./data/household_power_consumption.txt", sep = ";", header = T, na.strings = "?")
+data$Datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+
+histdata <- data[((data$Datetime >= '2007-02-01') & (data$Datetime <= '2007-02-03')),]
+
+png("./data/plot2.png", width=480, height=480, bg="white") 
+
+plot(histdata$Datetime,histdata$Global_active_power,type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+dev.off()
---
+++ figure/plot4.R
@@ -0,0 +1,35 @@
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if (!file.exists("./data/household_power_consumption.txt")) 
+{
+  if(!file.exists("./data")) { dir.create("./data")}
+  download.file(fileurl, destfile="./data/power_data.zip")
+  unzip("./data/power_data.zip", overwrite = T, exdir = "./data")
+}
+
+data <- read.table("./data/household_power_consumption.txt", sep = ";", header = T, na.strings = "?")
+data$Datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+
+histdata <- data[((data$Datetime >= '2007-02-01') & (data$Datetime <= '2007-02-03')),]
+
+png("./data/plot4.png", width=480, height=480, bg="white") 
+
+par(mfrow = c(2, 2))
+
+# plot 1 (Active Power - Top Left)
+plot(histdata$Datetime, histdata$Global_active_power, type = "l", ylab = "Global Active Power",xlab = "")
+
+# plot 2 (Voltage - Top Right)
+plot(histdata$Datetime, histdata$Voltage, type = "l", ylab = "Voltage", xlab = "datetime")
+
+# plot 3 (Metering  - Bottom Left)
+plot(histdata$Datetime, histdata$Sub_metering_1, type = "l", ylab = "Energy sub metering",xlab = "", col = "black")
+points(histdata$Datetime, histdata$Sub_metering_2, type = "l", xlab = "", ylab = "Sub_metering_2",col = "red")
+points(histdata$Datetime, histdata$Sub_metering_3, type = "l", xlab = "", ylab = "Sub_metering_3",col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n")
+
+# plot 4 (Reactive Power - Bottom Right)
+plot(histdata$Datetime, histdata$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power", ylim = c(0, 0.5))
+
+
+dev.off()
